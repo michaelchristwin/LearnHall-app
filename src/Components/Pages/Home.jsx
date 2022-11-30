2cf932007da8685ff112e7747faae24f76f7c2ac
michaelchristwin <kelechukwuchristwin@gmail.com>
2022-11-30 21:44:38 +0100

    init homepage

--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -1,17 +1,42 @@
 import Leaf from "../img/Vector.png";
-import Ellipse from "../img/Ellipse.png";
+//import Ellipse from "../img/Ellipse.png";
 import Name from "../img/Group.png";
 import "../CSS/styles.css";
 
 function Home() {
   return (
     <main>
-      <section>
-        <img src={Ellipse} alt="ellipse" />
-        <span className="d-flex">
+      <section className="top d-flex">
+        <span className="d-flex nav-brand">
           <img src={Leaf} alt="Learnhall Logo" className="logo" />
           <img src={Name} alt="Brand Name" className="brand" />
         </span>
+        <nav className="navbar navbar-expand-lg bg-white" id="nav">
+          <div className="container-fluid">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link active" href="#">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Book a Session
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Become a Tutor
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Blog
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
       </section>
     </main>
   );
