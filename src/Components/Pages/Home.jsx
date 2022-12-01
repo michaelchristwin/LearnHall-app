fb87d76d7ff00cd976fb3c103523ea90ad1f0946
michaelchristwin <kelechukwuchristwin@gmail.com>
2022-12-01 05:21:11 +0100

    Arranged Navbar

--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -1,18 +1,24 @@
 import Leaf from "../img/Vector.png";
-//import Ellipse from "../img/Ellipse.png";
+import SemiCircle from "../img/topEllipse.png";
 import Name from "../img/Group.png";
+import Student from "../img/student.png";
+import Rect from "../img/rectangle.png";
 import "../CSS/styles.css";
 
 function Home() {
   return (
     <main>
       <section className="top d-flex">
-        <span className="d-flex nav-brand">
-          <img src={Leaf} alt="Learnhall Logo" className="logo" />
-          <img src={Name} alt="Brand Name" className="brand" />
-        </span>
-        <nav className="navbar navbar-expand-lg bg-white" id="nav">
-          <div className="container-fluid">
+        <img src={SemiCircle} alt="" className="draw1" />
+
+        <nav className="navbar navbar-expand-lg bg-white p-4" id="nav">
+          <div className="container-fluid ">
+            <a class="navbar-brand" href="#">
+              <span className="d-flex nav-brand">
+                <img src={Leaf} alt="Learnhall Logo" className="logo" />
+                <img src={Name} alt="Brand Name" className="brand" />
+              </span>
+            </a>
             <ul className="navbar-nav">
               <li className="nav-item">
                 <a className="nav-link active" href="#">
@@ -37,6 +43,23 @@ function Home() {
             </ul>
           </div>
         </nav>
+      </section>
+      <section className="service">
+        <h1 className="service-title">Tutoring Done Right</h1>
+        <span>Private in-person & Online tutoring</span>
+        <aside>
+          If you want to learn an academic subject or study for an exam, we want
+          to be the company you choose. Our tutors are bright and enthusiastic
+          and seek out to bring the best of an individual learners potential in
+          both academics and live.
+        </aside>
+        <button className="btn">Book a Session</button>
+      </section>
+      <section className="">
+        <img src={Rect} alt="Rect" className="rectangle" />
+        <div className="student">
+          <img src={Student} alt="Student" className="stud-img" />
+        </div>
       </section>
     </main>
   );
