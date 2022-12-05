e8ba10750f83741a420f9d37e7ecbbcbf64d7bb7
michaelchristwin <kelechukwuchristwin@gmail.com>
2022-12-05 06:46:56 +0100

    deploy gh-page

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import reportWebVitals from "./reportWebVitals";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
