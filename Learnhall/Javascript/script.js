cbca0e717e0056bb38314194df6c99652832fa1b
Nelani Maluka <malukanelani@gmail.com>
2023-02-05 19:01:31 +0200

    Added the no hamburger querry

--- Learnhall/Javascript/script.js
+++ Learnhall/Javascript/script.js
@@ -1,5 +1,21 @@
-
 $(function () {
+    
+    function no_hamburger(){
+
+        let querry1 = window.matchMedia("(min-width: 1440px)");
+        let querry2 = window.matchMedia("(min-width: 960px) and (max-width: 1440px)");
+
+        if (querry1.matches) {
+            $(".hamburger-menu-open").css('display', 'none');
+            $(".hamburger-menu-close").css('display', 'none');
+            $(".nav-bar-hamburger-menu").css('display', 'none');
+        };
+        if (querry2.matches) {
+            $(".hamburger-menu-open").css('display', 'none');
+            $(".hamburger-menu-close").css('display', 'none');
+            $(".nav-bar-hamburger-menu").css('display', 'none');
+        };
+    }
     
     /* Hamburger menu function */
     $(".hamburger-menu-open").click(function (){
@@ -8,6 +24,7 @@ $(function () {
         $(".hamburger-menu-open").css('display', 'none');
         $(".hamburger-menu-close").css('display', 'flex');
         $(".nav-bar-hamburger-menu").css('display', 'block');
+        no_hamburger()
 
         /* Closing the menu */
         $(".hamburger-menu-close").click(function (){
@@ -15,6 +32,7 @@ $(function () {
             $(".hamburger-menu-open").css('display', 'flex');
             $(".hamburger-menu-close").css('display', 'none');
             $(".nav-bar-hamburger-menu").css('display', 'none');
+            no_hamburger()
         });
     });
 
